2c8db3aa2f6aaea639815f8f8e2cf140d5d8f2fd
stnj <stnj@demant.com>
2023-11-02 10:59:56 +0100

    version2 added

--- tools/nn_modules.py
+++ tools/nn_modules.py
@@ -1,8 +1,23 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchaudio.functional as AF
 from math import pi
 
+import numpy as np
+import pyfilterbank
+import operator
+from scipy._lib._util import float_factorial
+
+def _hz_to_erb(hz):
+    """
+    Utility for converting from frequency (Hz) to the
+    Equivalent Rectangular Bandwidth (ERB) scale
+    ERB = frequency / EarQ + minBW
+    """
+    EarQ = 9.26449
+    minBW = 24.7
+    return hz / EarQ + minBW
 
 class FIRFilter1D(nn.Module):
     """Implements an arbitrary phase FIR filter"""
@@ -16,7 +31,9 @@ class FIRFilter1D(nn.Module):
     def forward(self, x):
         # FIR filter implemented with a conv1d layer
         filtered_signal = F.conv1d(
-            x, self.filter_taps.view(1, 1, -1), padding=self.num_taps - 1, #bias=False
+            x,
+            self.filter_taps.view(1, 1, -1),
+            padding=self.num_taps - 1,  # bias=False
         )
         return filtered_signal
 
@@ -44,35 +61,24 @@ class FIRFilter1DLinearPhaseI(nn.Module):
         self.filter_taps = torch.cat([mirrored_taps, self.learnable_taps])
         # Apply the FIR filter operation with a conv1d layer
         filtered_signal = torch.conv1d(
-            x, self.filter_taps.view(1, 1, -1), padding=self.num_taps - 1, #bias=False
+            x,
+            self.filter_taps.view(1, 1, -1),
+            padding=self.num_taps - 1,  # bias=False
         )
 
         return filtered_signal
 
 
-class GammaToneFilter(nn.Module):
+class GammaToneFIRFilter(nn.Module):
     """Generates a gammatone filter as a torch module (non-trainable)."""
 
-    def __init__(self, duration, fc_hz, fs_hz, delta_q=0):
-        super(GammaToneFilter, self).__init__()
+    def __init__(self, center_frequency, samplerate, band_width_factor=1.0):
+        super(GammaToneFIRFilter, self).__init__()
 
-        self.duration = duration
-        self.fc_hz = fc_hz
-        self.fs_hz = fs_hz
-        self.delta_q = delta_q
+        self.center_frequency = center_frequency
+        self.samplerate = samplerate
+        self.delta_q = band_width_factor
 
-        ERB = 24.7 + (1 + self.delta_q) * fc_hz * 0.108
-
-        b = 1.018 * ERB
-        a = 6 / (-2 * pi * b) ** 4
-
-        t = torch.arange(0, duration, 1 / fs_hz)
-        self.impulse_response = (
-            a**-1
-            * t**3
-            * torch.cos(2 * pi * fc_hz * t)
-            * torch.exp(-2 * pi * b * t)
-        )
 
     def forward(self, x):
         # Apply the gamma-tone filter to the input signal
@@ -81,27 +87,145 @@ class GammaToneFilter(nn.Module):
             x,
             self.impulse_response.view(1, 1, -1),
             padding=len(self.impulse_response) - 1,
-            #bias=False,
+            # bias=False,
         )
         return torch.flip(filtered_signal, dims=[-1])
+    
+def design_gammatone(freq, samplerate, ftype, order=4, numtaps=32, band_width_factor=1.0):
+    """
+    Adapted from scipy.signal.gammatone  https://github.com/scipy/scipy/blob/v1.11.3/scipy/signal/_filter_design.py#L5393-L5574
+    """
+    # Converts freq to float
+    freq = float(freq)
+
+    # Set sampling rate if not passed
+    if samplerate is None:
+        samplerate = 2
+    samplerate = float(samplerate)
+
+    # Check for invalid cutoff frequency or filter type
+    ftype = ftype.lower()
+    filter_types = ['fir', 'iir']
+    if not 0 < freq < samplerate / 2:
+        raise ValueError("The frequency must be between 0 and {}"
+                         " (nyquist), but given {}.".format(samplerate / 2, freq))
+    if ftype not in filter_types:
+        raise ValueError('ftype must be either fir or iir.')
+
+    # Calculate FIR gammatone filter
+    if ftype == 'fir':
+        # Set order and numtaps if not passed
+        if order is None:
+            order = 4
+        order = operator.index(order)
+
+        if numtaps is None:
+            numtaps = max(int(samplerate * 0.015), 15)
+        numtaps = operator.index(numtaps)
+
+        # Check for invalid order
+        if not 0 < order <= 24:
+            raise ValueError("Invalid order: order must be > 0 and <= 24.")
+
+        # Gammatone impulse response settings
+        t = np.arange(numtaps) / samplerate
+        bw = 1.019 * _hz_to_erb(freq) * band_width_factor
+
+        # Calculate the FIR gammatone filter
+        b = (t ** (order - 1)) * np.exp(-2 * np.pi * bw * t)
+        b *= np.cos(2 * np.pi * freq * t)
+
+        # Scale the FIR filter so the frequency response is 1 at cutoff
+        scale_factor = 2 * (2 * np.pi * bw) ** (order)
+        scale_factor /= float_factorial(order - 1)
+        scale_factor /= samplerate
+        b *= scale_factor
+        a = [1.0]
+        return b, a
+    
+    elif ftype == 'iir':
+        raise NotImplementedError()
+    else:
+        raise ValueError('Invalid filter type')
 
 
-class GammaToneFilterbank(nn.Module):
+class GammaToneIIRFilter(nn.Module):
+    """Generates a gammatone filter as a torch module (non-trainable)."""
+
+    def __init__(self, center_frequency, samplerate, band_width_factor=1.0):
+        super(GammaToneIIRFilter, self).__init__()
+        self.center_frequency = center_frequency
+        self.samplerate = samplerate
+        self.band_width_factor = band_width_factor
+
+        b, a = pyfilterbank.gammatone.design_filter(
+            sample_rate=self.samplerate,
+            order=32,
+            centerfrequency=self.center_frequency,
+            band_width=None,
+            band_width_factor=self.band_width_factor,
+            attenuation_half_bandwidth_db=-3,
+        )
+        
+        self.b = torch.complex(torch.Tensor([b[0], 0.0]), torch.Tensor([0,0]))
+        self.a = torch.complex(torch.Tensor(np.real(a)), torch.Tensor(np.imag(a)))
+
+    def forward(self, x):
+        # Apply the gamma-tone filter to the input signal
+        x_real = AF.lfilter(x, torch.real(self.a), torch.real(self.b))
+        x_imag = AF.lfilter(x, torch.imag(self.a), torch.imag(self.b))
+        
+        print(x_imag)
+        x = torch.sqrt(x_real**2)
+        return x
+
+
+class GammaToneFIRFilterbank(nn.Module):
     """Generates a gammatone filterbank with given center frequencies
     as a torch module (non-trainable).
     """
 
-    def __init__(self, duration, center_frequencies, fs_hz, delta_q=0):
+    def __init__(self, duration, center_frequencies, samplerate, delta_q=0):
         """_summary_
 
         Args:
             center_frequencies (list[int,...]): list of center frequencies
-            fs_hz (_type_): sampling frequency
+            samplerate (float): sampling frequency
             delta_q (int, optional): numerical value that widens ERBs. Defaults to 0.
         """
-        super(GammaToneFilterbank, self).__init__()
+        super(GammaToneFIRFilterbank, self).__init__()
         self.filters = nn.ModuleList(
-            [GammaToneFilter(duration, fc, fs_hz, delta_q) for fc in center_frequencies]
+            [
+                GammaToneFIRFilter(duration, fc, samplerate, delta_q)
+                for fc in center_frequencies
+            ]
+        )
+
+    def forward(self, x):
+        # Apply the entire filterbank to the input signal
+        outputs = [filter(x) for filter in self.filters]
+        return outputs
+
+
+class GammaToneIIRFilterbank(nn.Module):
+    """Generates a gammatone filterbank with given center frequencies
+    as a torch module (non-trainable).
+    """
+
+    def __init__(self, center_frequencies, samplerate, band_width_factor=1.0):
+        """_summary_
+
+        Args:
+            center_frequencies (list[int,...]): list of center frequencies
+            samplerate (_type_): sampling frequency
+            delta_q (int, optional): numerical value that widens ERBs. Defaults to 0.
+        """
+        super(GammaToneIIRFilterbank, self).__init__()
+        self.filters = nn.ModuleList(
+            [
+                GammaToneIIRFilter(fc, samplerate, band_width_factor)
+                for fc in center_frequencies
+            ]
         )
 
     def forward(self, x):
@@ -126,12 +250,39 @@ class HearingModel(nn.Module):
         self.delta_q = delta_q
         self.duration = 0.25  # fixed
 
-        self.gamma_bank = GammaToneFilterbank(
+        self.gfb = GammaToneFIRFilterbank(
             self.duration, self.center_frequencies, self.samplerate, self.delta_q
         )
 
     def forward(self, x):
-        signals = self.gamma_bank(x)
+        signals = self.gfb(x)
+        out = torch.zeros_like(signals[0])
+        for signal in signals:
+            out += signal  # Sum outputs of filterbank
+        return torch.div(out, len(signals))  # ? how to fix this?
+
+
+class HearingModel_v2(nn.Module):
+    """Hearing model"""
+
+    def __init__(self, center_frequencies, samplerate, band_width_factor=1.0):
+        """
+        Args:
+            samplerate (int):
+            center_frequencies (list[float,..]): list of center frequencies
+            delta_q (float): delta Q factor to be added to normal base
+        """
+        super(HearingModel_v2, self).__init__()
+        self.samplerate = samplerate
+        self.center_frequencies = center_frequencies
+        self.band_width_factor = band_width_factor
+
+        self.gfb = GammaToneIIRFilterbank(
+            self.center_frequencies, self.samplerate, self.band_width_factor
+        )
+
+    def forward(self, x):
+        signals = self.gfb(x)
         out = torch.zeros_like(signals[0])
         for signal in signals:
             out += signal  # Sum outputs of filterbank
@@ -176,13 +327,13 @@ class MyModel_v1(nn.Module):
             input=out_NH, pad=[0, self.num_taps - 1], mode="constant", value=0
         )
         return out_NH, out_HI
-    
+
+
 class MyModel_v2(nn.Module):
     """My model"""
 
-    def __init__(self, num_taps, samplerate, center_frequencies, delta_q=1.0):
-        """This model uses two serial linear phaes fir filters
-
+    def __init__(self, num_taps, samplerate, center_frequencies, band_width_factor=5.0):
+        """This model uses
         Args:
             num_taps (int): number of taps for the FIR filter
             samplerate (int):
@@ -192,27 +343,21 @@ class MyModel_v2(nn.Module):
         self.num_taps = num_taps
         self.samplerate = samplerate
         self.center_frequencies = center_frequencies
-        self.delta_q = delta_q
-        self.duration = 0.25  # fixed at 1
+        self.band_width_factor = band_width_factor
 
-        self.normal_model = HearingModel(
-            samplerate=samplerate, center_frequencies=self.center_frequencies
+        self.normal_model = HearingModel_v2(self.center_frequencies, self.samplerate)
+
+        self.impaired_model = HearingModel_v2(
+            self.center_frequencies, self.samplerate, self.band_width_factor
         )
 
-        self.impaired_model = HearingModel(
-            samplerate=self.samplerate,
-            center_frequencies=self.center_frequencies,
-            delta_q=self.delta_q,
-        )
-
-        self.fir1 = FIRFilter1DLinearPhaseI(self.num_taps)
-        self.fir2 = FIRFilter1DLinearPhaseI(self.num_taps)
+        self.hearing_aid_model = FIRFilter1D(self.num_taps)
 
     def forward(self, x):
-        out_HI = self.fir2(self.fir1(self.impaired_model(x)))
+        out_HI = self.hearing_aid_model(self.impaired_model(x))
         out_NH = self.normal_model(x)
         # Zero pad normal hearing to match HI
         out_NH = F.pad(
-            input=out_NH, pad=[0, self.num_taps*2 - 2], mode="constant", value=0
+            input=out_NH, pad=[0, self.num_taps * 2 - 2], mode="constant", value=0
         )
         return out_NH, out_HI
